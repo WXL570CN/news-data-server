716af91a48e913e880f2ef482968790a3be4390d
snail <570@snail.com>
2020-03-30 21:21:21 +0800

    新增新闻详情存储路径

--- model/constant.js
+++ model/constant.js
@@ -5,5 +5,7 @@ const monthD = date.format(new Date(), 'MMDD')
 // const time = '20200202'
 // 每日新闻url
 const url = `http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=www_www_all_suda_suda&top_time=${yearMD}&top_show_num=100&top_order=DESC&js_var=all_1_data01`;
+// 新闻详情存储路径
+const articlePath = __dirname + `/public/article/${yearMD}.txt`
 
-module.exports = {yearMD, monthD, url}
+module.exports = { yearMD, monthD, url, articlePath }
